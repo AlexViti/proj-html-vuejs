ad865dec69583d0fb1cc3d2f57dc181f8cbe437d
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-04-01 21:14:08 +0200

    Add first fixed botton

--- src/components/FixedButtons.vue
+++ src/components/FixedButtons.vue
@@ -1,12 +1,21 @@
 <template>
 <div class="fixed-btns">
-	<button>bottone</button>
+	<button>
+		<div class="icons">
+			<rectangle-history />
+			<rectangle-history />
+		</div>
+		<span>Demos</span>
+	</button>
 </div>
 </template>
 
 <script>
+import RectangleHistory from './utility/RectangleHistory.vue'
+
 export default {
-	name: 'FixedButtons'
+	name: 'FixedButtons',
+	components: { RectangleHistory }
 }
 </script>
 
@@ -17,5 +26,29 @@ export default {
 	position: absolute;
 	top: $headerHeight + 20px;
 	right: 20px;
+}
+
+button {
+	color: $iconGray;
+
+	.icons {
+		font-size: 2.5rem;
+		max-width: 3.5rem;
+		white-space: nowrap;
+		overflow: hidden;
+		padding-left: .5rem;
+
+		> * {
+			transition: transform 0.2s;
+		}
+
+		* + * {
+		margin-left: 1em;
+		}
+	}
+
+	&:hover .icons > * {
+		transform: translateX(calc(-100% - 1em));
+	}
 }
 </style>
